fc54ffd387bc35571c51f62aaf4fb0b33715cb9a
Rajesh Karra <23515917+r-karra@users.noreply.github.com>
2026-09-17 15:56:20 +0530

    Configure production and dev environments to secure HTTPS AWS API Gateway proxy endpoint

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://jbac-backend-env.eba-rdpqwigp.ap-southeast-2.elasticbeanstalk.com',
+  apiUrl: 'https://tit71pnnuc.execute-api.ap-southeast-2.amazonaws.com/default',
   ebApiUrl: 'http://jbac-backend-env.eba-rdpqwigp.ap-southeast-2.elasticbeanstalk.com',
   proxyApiUrl: '/dashboardapi/',
   fallbackApiUrl: 'https://jbac.in:9762/dashboardapi/'
